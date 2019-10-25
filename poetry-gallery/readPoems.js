51d0a10b2a932ba27d5806878139cd7608abd7ce
Ben <benjaminjflowers@gmail.com>
2019-10-25 09:56:08 -0500

    succesfully logging all contents of one txt file

--- poetry-gallery/readPoems.js
+++ poetry-gallery/readPoems.js
@@ -1,5 +1,6 @@
 const fs = require('file-system');
-
+const copyDirectory = './copy'
+// returns list of files found at directory
 const getFiles = (url) => {
   return new Promise((resolve, reject) => {
     fs.readdir(url, (err, files) => {
@@ -12,14 +13,13 @@ const getFiles = (url) => {
   })
 }
 
-getFiles('./copy').then((files) => {console.log(files)});
 
-// let message = {
-//   message1: 'hello',
-//   message2: 'goodbye'
-// }
-
-// module.exports = {
-//   // message: message,
-//   getData: getData
-// }
+getFiles(copyDirectory).then((files) => {
+  fs.readFile(copyDirectory + '/' + files[1], 'utf8', (err, data) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(data);
+    }
+  });
+});
