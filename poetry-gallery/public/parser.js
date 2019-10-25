8ae81f36270e00374624bbae5acc4c028843b071
Ben <benjaminjflowers@gmail.com>
2019-10-25 15:48:44 -0500

    rendering all titles in the left panel

--- poetry-gallery/public/parser.js
+++ poetry-gallery/public/parser.js
@@ -22,25 +22,19 @@ const setPoems = async (url) => {
   for(let i = 0; i < keys.length; i++){
     let poemTitle = formatTitle(keys[i]);
     let poemContent = collection.poems[keys[i]];
-    let poemCard = document.createElement('div');
-
-    poemCard.classList.add('poem-card');
-    poemCard.id = `poem${i}`;
+    let titlesDiv = document.body.querySelector('.titles');
 
     let titleNode = document.createElement('h1');
     titleNode.classList.add('poem-title');
     titleNode.id = `poemTitle${i}`;
     titleNode.innerText = poemTitle;
+    titleNode.style.textAlign = 'center';
+    titlesDiv.appendChild(titleNode);
 
     let contentNode = document.createElement('p');
     contentNode.classList.add('poem-content');
     contentNode.id = `poem${i}`;
     contentNode.innerText = poemContent;
-
-    poemCard.appendChild(titleNode);
-    poemCard.appendChild(contentNode);
-
-    document.body.querySelector('.main').appendChild(poemCard);
   }
 }
 
